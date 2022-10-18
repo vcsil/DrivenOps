b343380a554a2f13ff49ab8efa804eca81cc5f6a
vcsil <vcsilva@poli.ufrj.br>
2022-10-17 21:21:35 -0300

    :boom: fix: restart project

--- back-end/prisma/migrations/20220804114255_create_student_model/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "students" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-
-    CONSTRAINT "students_pkey" PRIMARY KEY ("id")
-);
--- back-end/src/server.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-import app from './app.js';
-
-dotenv.config();
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Servidor funfando na porta: ${PORT}`);
-});
